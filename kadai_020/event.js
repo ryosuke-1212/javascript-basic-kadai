65b4a0c8f2064c22c81b1dbbb3a8c823f38aa62a
ryosuke-1212 <shirataki523@gmail.com>
2024-08-25 13:29:22 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,16 @@
+const addBtn = document.getElementById('btn');
+
+const parentList = document.getElementById('text');
+
+addBtn.addEventListener('click', () => {
+
+  const childList = document.createElement('text');
+  childList.textContent = 'ボタンをクリックしました';
+
+  while (parentList.firstChild) {
+    parentList.removeChild(parentList.firstChild);
+  }
+
+  // childListを追加する
+  parentList.appendChild(childList);
+});
